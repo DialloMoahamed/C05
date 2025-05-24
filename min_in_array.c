0efa9c89ad89c4df5c4fdfbb4fe7538573a000de
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-24 03:54:48 +0100

    Commit

--- min_in_array.c
+++ min_in_array.c
@@ -1,40 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main() {
-    int n, i;
-    if (scanf("%d", &n) != 1 || n <= 0) {
-        printf("Taille du tableau invalide.\n");
-        return 1;
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int tab[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &tab[i]);
     }
-
-    int *tableau = (int *)malloc(n * sizeof(int));
-    if (tableau == NULL) {
-        printf("Erreur d'allocation mémoire.\n");
-        return 1;
-    }
-
-    printf("Entrez les éléments du tableau : ");
-    for (i = 0; i < n; i++) {
-        if (scanf("%d", &tableau[i]) != 1) {
-            printf("Erreur lors de la lecture des éléments.\n");
-            free(tableau);
-            return 1;
+    int min = tab[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (tab[i] < min)
+        {
+            min = tab[i];
         }
     }
-
-    if (n == 0) {
-        printf("Le tableau est vide.\n");
-    } else {
-        int min = tableau[0];
-        for (i = 1; i < n; i++) {
-            if (tableau[i] < min) {
-                min = tableau[i];
-            }
-        }
-        printf("Le minimum est : %d\n", min);
-    }
-
-    free(tableau);
+    printf("Le minimum est : %d\n", min);
     return 0;
 }
