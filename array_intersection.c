07b67e2b731c5512ca8423ac5e44f19dd87a9d70
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-24 04:29:27 +0100

    commit

--- array_intersection.c
+++ array_intersection.c
@@ -1,45 +1,40 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int n;
     scanf("%d", &n);
     int tab1[n];
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         scanf("%d", &tab1[i]);
-    }
 
     int m;
     scanf("%d", &m);
     int tab2[m];
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < m; i++)
         scanf("%d", &tab2[i]);
-    }
-
-    int inter[n < m ? n : m];
-    int taille_inter = 0;
 
     printf("Intersection :");
 
-    for (int i = 0; i < n; i++) {
-        // Vérifier si tab1[i] a déjà été affiché (dans inter)
+    for (int i = 0; i < n; i++)
+    {
         int deja_affiche = 0;
-        for (int k = 0; k < taille_inter; k++) {
-            if (inter[k] == tab1[i]) {
+        for (int k = 0; k < i; k++)
+        {
+            if (tab1[k] == tab1[i])
+            {
                 deja_affiche = 1;
                 break;
             }
         }
-        if (deja_affiche) {
-            continue; // on ignore cet élément car déjà affiché
-        }
+        if (deja_affiche)
+            continue;
 
-        // Chercher tab1[i] dans tab2
-        for (int j = 0; j < m; j++) {
-            if (tab1[i] == tab2[j]) {
-                // On affiche et on stocke pour ne pas répéter
+        for (int j = 0; j < m; j++)
+        {
+            if (tab1[i] == tab2[j])
+            {
                 printf(" %d", tab1[i]);
-                inter[taille_inter++] = tab1[i];
-                // Marquer tab2[j] comme utilisé
                 tab2[j] = -99999;
                 break;
             }
@@ -47,6 +42,5 @@ int main() {
     }
 
     printf("\n");
-
     return 0;
 }
