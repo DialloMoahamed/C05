1886222d107b0f90c51cc14b2891564d309b761b
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-24 03:48:52 +0100

    Commit

--- min_in_array.c
+++ min_in_array.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#define MAX 1000
 
 int main()
 {
     int n;
     scanf("%d", &n);
 
-    int tab[n];
+    if (n > MAX || n <= 0)
+        return 1;
+
+    int tab[MAX];
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &tab[i]);
