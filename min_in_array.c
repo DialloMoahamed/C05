2798f6e5f44f5d862bbdeb10e31e628fea66217a
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-24 03:56:30 +0100

    Commit

---
+++ min_in_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int tab[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &tab[i]);
+    }
+    int min = tab[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (tab[i] < min)
+        {
+            min = tab[i];
+        }
+    }
+    printf("Le minimum est : %d\n", min);
+    return 0;
+}
