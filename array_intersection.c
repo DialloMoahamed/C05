cf26d2cbfe761454c641c0e4fe25623e1f8c7c63
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-24 04:23:36 +0100

    commit

--- array_intersection.c
+++ array_intersection.c
@@ -5,35 +5,44 @@ int main()
     int n;
     scanf("%d", &n);
     int tab1[n];
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         scanf("%d", &tab1[i]);
-
     }
+
     int m;
     scanf("%d", &m);
     int tab2[m];
-    for (int i = 0; i < m; i++)
-    {
+    for (int i = 0; i < m; i++) {
         scanf("%d", &tab2[i]);
-
     }
+
+    int inter[n < m ? n : m];
+    int taille_inter = 0;
+
     printf("Intersection :");
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if (tab1[i] == tab2[j])
-            {
+    for (int i = 0; i < n; i++) {
+        // Vérifier si tab1[i] a déjà été affiché
+        int deja_affiche = 0;
+        for (int k = 0; k < taille_inter; k++) {
+            if (inter[k] == tab1[i]) {
+                deja_affiche = 1;
+                break;
+            }
+        }
+        if (deja_affiche) {
+            continue;
+        }
+
+        for (int j = 0; j < m; j++) {
+            if (tab1[i] == tab2[j]) {
                 printf(" %d", tab1[i]);
+                inter[taille_inter++] = tab1[i];
                 tab2[j] = -99999;
                 break;
             }
-            
         }
-        
-    }    
+    }
     printf("\n");
-    
+
     return 0;
 }
