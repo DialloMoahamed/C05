28c05ad20c5232340e73c3cffff79c39467c247b
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-24 01:49:20 +0100

    Commit

---
+++ array_intersection.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int tab1[n] ;
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &tab1[i]);
+    }
+    int m;
+    scanf("%d", &m);
+    int tab2[m];
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%d", &tab2[i]);
+    }
+    printf("Intersection : ");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (tab1[i] == tab2[j])
+            {
+                printf("%d ", tab1[i]);
+            }
+            
+        }
+        
+    }
+    
+    
+    return 0;
+}
