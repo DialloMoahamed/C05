1f14e40856cde0981cc6315bfe0a2be0c0a40352
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-25 03:51:26 +0100

    Commit

--- array_intersection.c
+++ array_intersection.c
@@ -1,37 +1,33 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-
-
-int main()
-{
-    int N1, N2, n[100];
-    int i, x;
+int main() {
+    int N1, N2;
+    int arr1[100], arr2[100];
+    bool seen[1001] = { false }; 
+    bool intersection[1001] = { false };
 
     scanf("%d", &N1);
-
-    for (i = 0; i < N1; i++)
-        {
-            scanf("%d", &x);
-
-            n[x] = 1;
-        }
+    for (int i = 0; i < N1; i++) {
+        scanf("%d", &arr1[i]);
+        seen[arr1[i]] = true;
+    }
 
     scanf("%d", &N2);
-
-    for (i = 0; i < N2; i++)
-        {
-            scanf("%d", &x);
-
-            n[x] |= 2;
+    for (int i = 0; i < N2; i++) {
+        scanf("%d", &arr2[i]);
+        if (seen[arr2[i]]) {
+            intersection[arr2[i]] = true; 
         }
+    }
 
     printf("Intersection : ");
-
-    for (i = 0; i <= 100; i++)
-    {
-        if (n[i] == 3)
-        printf("%d ", i);
+    for (int i = 0; i <= 1000; i++) {
+        if (intersection[i]) {
+            printf("%d ", i);
+        }
     }
+    printf("\n");
 
     return 0;
 }
