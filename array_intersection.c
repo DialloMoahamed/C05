3566501ee190d0ad49597041a5dc29a4c892ee91
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-26 03:08:18 +0100

    Commit

--- array_intersection.c
+++ array_intersection.c
@@ -1,37 +1,46 @@
 #include <stdio.h>
-#include <stdbool.h>
 
-#define MAX 1000
-
-int main() {
-    int compt1, compt2;
-    int tableau1[MAX], tableau2[MAX];
-    bool deja_affiche[2001] = {false}; 
-
-    scanf("%d", &compt1);
-    for (int i = 0; i < compt1; i++) {
-        scanf("%d", &tableau1[i]);
+int main()
+{
+    int n1, n2;
+    scanf("%d", &n1);
+    scanf("%d", &n2);
+    int tab2[n2];
+    int tab1[n1];
+    for (int i = 0; i < n1; i++)
+    {
+        scanf("%d", &tab1[i]);
     }
-
-    scanf("%d", &compt2);
-    for (int i = 0; i < compt2; i++) {
-        scanf("%d", &tableau2[i]);
+    for (int i = 0; i < n2; i++)
+    {
+        scanf("%d", &tab2[i]);
     }
-
-    printf("Intersection : ");
-    for (int i = 0; i < compt1; i++) {
-        for (int j = 0; j < compt2; j++) {
-            if (tableau1[i] == tableau2[j]) {
-                int val = tableau1[i] + 1000;
-                if (!deja_affiche[val]) {
-                    printf("%d", tableau1[i]);
-                    deja_affiche[val] = true;
-                }
+    printf("Intersection :");
+    for (int i = 0; i < n1; i++)
+    {
+        int presentDanstab2 = 0;
+        for (int j = 0; j < n2; j++)
+        {
+            if (tab1[i] == tab2[j])
+            {
+                presentDanstab2 = 1;
                 break;
             }
         }
+        int déjà_affiché = 0;
+        for (int k = 0; k < i; k++)
+        {
+            if (tab1[i] == tab1[k])
+            {
+                déjà_affiché = 1;
+                break;
+            }
+        }
+        if (presentDanstab2 && !déjà_affiché)
+        {
+            printf(" %d", tab1[i]);
+        }
     }
-
     printf("\n");
     return 0;
 }
