39f5201d2194851b91e2f36767f3514755bf203b
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-29 19:50:48 +0100

    Commit

--- intersection_arrays.c
+++ intersection_arrays.c
@@ -1,76 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(void)
+int main()
 {
-    int n1, n2;
-
-    // Lire la taille du premier tableau
-    scanf("%d", &n1);
-
-    // Allouer dynamiquement le premier tableau
-    int *tab1 = malloc(n1 * sizeof(int));
-
-    // Remplir le premier tableau
-    for (int i = 0; i < n1; i++)
+    int n;
+    scanf("%d", &n);
+    int tab1[n] ;
+    
+    for (int i = 0; i < n; i++)
     {
         scanf("%d", &tab1[i]);
     }
-
-    // Lire la taille du deuxième tableau
-    scanf("%d", &n2);
-
-    // Allouer dynamiquement le deuxième tableau
-    int *tab2 = malloc(n2 * sizeof(int));
-
-    // Remplir le deuxième tableau
-    for (int i = 0; i < n2; i++)
+    int m;
+    scanf("%d", &m);
+    int tab2[m];
+    for (int i = 0; i < m; i++)
     {
         scanf("%d", &tab2[i]);
     }
-
-    // Affichage du titre (respecter la majuscule et l’espace)
     printf("Intersection : ");
-
-    // Parcourir le premier tableau
-    for (int i = 0; i < n1; i++)
+    for (int i = 0; i < n; i++)
     {
-        int est_dans_tab2 = 0;
-
-        // Chercher si tab1[i] est dans tab2
-        for (int j = 0; j < n2; j++)
+        for (int j = 0; j < m; j++)
         {
             if (tab1[i] == tab2[j])
             {
-                est_dans_tab2 = 1;
-                break;
+                printf("%d ", tab1[i]);
             }
+            
         }
-
-        // Vérifier si on a déjà affiché ce nombre (doublon)
-        int deja_affiche = 0;
-        for (int k = 0; k < i; k++)
-        {
-            if (tab1[k] == tab1[i])
-            {
-                deja_affiche = 1;
-                break;
-            }
-        }
-
-        // Si c’est dans tab2 et pas encore affiché, on l’affiche
-        if (est_dans_tab2 && !deja_affiche)
-        {
-            printf("%d ", tab1[i]);
-        }
+        
     }
-
-    // Retour à la ligne à la fin
     printf("\n");
-
-    // Libérer la mémoire
-    free(tab1);
-    free(tab2);
-
+    
     return 0;
 }
